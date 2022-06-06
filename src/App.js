79c9ac51087a47674789f0a47b9961da6db8bedd
Piotr Ligaj <piotr.ligaj24@gmail.com>
2022-06-06 18:39:58 +0200

    custom hook useLoginUser

--- src/App.js
+++ src/App.js
@@ -1,51 +1,30 @@
 import { Box, Container, createTheme, ThemeProvider } from '@mui/material'
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import NavBar from './components/NavBar';
 import { Routes, Route } from 'react-router-dom';
 import User from './pages/User';
 import AddPost from './pages/AddPost';
-import { auth, usersColRef, getUserBy } from './firebase';
 import Posts from './pages/Posts';
-import { useDispatch, useSelector } from 'react-redux';
-import { changeValue, setUser } from './store/userSlice';
-import { query, where } from 'firebase/firestore';
 import Comments from './pages/Comments';
 import PostPage from './pages/PostPage';
 import ModalOptions from './components/ModalOptions';
-import { onAuthStateChanged } from 'firebase/auth';
 import CreateUser from './pages/CreateUser';
 import Settings from './pages/Settings';
 import AlertComponent from './components/AlertComponent';
 import { useUser } from './hooks/useUser';
+import { useLoginUser } from './hooks/useLoginUser';
 
 function App() {
-  const dispatch = useDispatch();
-  const [uid, setUid] = useState(null);
   const [lastVisiblePost, setLastVisiblePost] = useState(null)
   const { settings } = useUser()
-  useEffect( () => {
-    const fetchLoggedUser = async () => {
-      const q = query(usersColRef, where("uid", "==", uid));
-      const response = await getUserBy(q)
-      if(uid && response[0]) dispatch(setUser(response[0]))
-      else if(uid) dispatch(changeValue({uid}))
-    }
-    if(uid){
-      fetchLoggedUser()
-    }
-  }, [uid] )
-
-  onAuthStateChanged(auth, async (currentUser) => {
-    if(currentUser){
-      setUid(currentUser.uid)
-    }
-  })
 
   const theme = createTheme({
     palette: {
       mode: `${ settings.darkTheme ? 'dark' : 'light'}`
     }
   })
+
+  useLoginUser()
   return (
     <ThemeProvider theme={theme}>
       <Container sx={{
